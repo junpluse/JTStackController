9142148c90be80058c57e555eafc5672124b9a25
Jun Tanaka <junpluse@gmail.com>
2011-05-08 01:09:00 +0900

    Initial Commit

---
+++ JTStackController/JTStackControllerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  JTStackControllerAppDelegate.h
+//  JTStackController
+//
+//  Created by Jun on 5/8/11.
+//  Copyright 2011 東京工科大学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTStackControllerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
